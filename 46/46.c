43033c93a9bac1ee8c6dce20ef2b708c8ec95ff6
Sriharsha <harshasrisri@gmail.com>
2013-12-30 15:52:11 -0700

    Solved 46

--- 46/46.c
+++ 46/46.c
@@ -2,66 +2,58 @@
 # include <string.h>
 
 #ifndef LIMIT
-#define LIMIT 10000000000
+#define LIMIT 10000
 #endif
 
-#ifndef LIMIT1
-#define LIMIT1 10000000000
-#endif
+unsigned char prime[LIMIT] = {' '};
+unsigned int squares[LIMIT];
 
-unsigned char prime[LIMIT/(sizeof(unsigned char) * 8 * 2) + 1] = {0};
-unsigned char goldbach[LIMIT1/(sizeof(unsigned char) * 8 * 2) + 1] = {0};
-
-#define is_prime(x) (!checkbit(prime,x))
-#define is_not_prime(x) (checkbit(prime,x))
-#define is_not_goldbach(x) (checkbit(goldbach,x))
-
-inline int checkbit (unsigned char array[], unsigned int pos) {
-	if (pos == 2) return 0;
-	if (!(pos & 1)) return 1;
-	return (array[pos / (sizeof(array[0]) * 8)]) & (1 << (pos % (sizeof(array[0]) * 8)));
-}
-
-inline void setbit (unsigned char array[], unsigned int pos) {
-	if (!(pos & 1)) return;
-	pos = pos >> 1;
-	(array[pos / (sizeof(array[0]) * 8)]) |= (1 << (pos % (sizeof(array[0]) * 8)));
-	return;
-}
+#define is_prime(x) (!prime[x])
 
 void prepare_primes () {
-	unsigned long int i = 2, j, temp;
-	unsigned long long temp1;
+	unsigned int i = 2, j, temp;
 
-	for (i = 3; i <= (LIMIT >> 1); i+=2) {
-		if (is_not_prime(i)) continue;
+	prime[0] = prime[1] = 1;
+
+	for (i = 3; i <= LIMIT; i+=2) {
+		if (!is_prime(i)) continue;
 		j = 2;
-		setbit(goldbach, i);
 		while ((temp = i * j) < LIMIT) {
-			if ((temp1 = i + 2 * (j - 1) * (j - 1)) < LIMIT1) {
-				setbit (goldbach, temp1);
-			}
-			if ((i == 3) && (j & 1)) {
-				setbit (prime, j + 1);
-				setbit (prime, j - 1);
-				setbit (goldbach, j + 1);
-				setbit (goldbach, j - 1);
-			}
-			setbit(prime, temp);
-			setbit(goldbach, temp);
+			prime[temp] = 1;
 			j++;
 		}
 	}
 }
 
+int is_square (int a) {
+	int i;
+	for (i = 1; i < LIMIT; i++)
+		if (squares[i] == a)
+			return 1;
+	return 0;
+}
+
 int main () {
-	int i = 3;
+	int i, j;
 
 	prepare_primes();
 
-	while (is_not_goldbach(i)) 
-		printf ("%d\n", i+=2);
+	for (i = 0; i < LIMIT; i++)
+		squares[i] = i * i;
 
-	printf ("%d\n", i);
+	for (i = 9; i < LIMIT; i+=2) {
+		if (is_prime(i)) continue;
+		for (j = 3; j < i; j+=2) {
+			if (!is_prime(j)) continue;
+			if ((i - j) & 1) continue;
+			if (!is_square((i - j) >> 1)) continue;
+			break;
+		}
+
+		if (j >= i) {
+			printf ("\n%d\n", i);
+			break;
+		}
+	}
 	return 0;
 }
